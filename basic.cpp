06f87291e4eb64c6c6226de5d450c67db60bc390
Nishant Rai <nishantrai11@outlook.com>
2022-09-26 23:03:44 +0530

    code of output and input including few demo of compiler warnings have been added

--- basic.cpp
+++ basic.cpp
@@ -1,9 +1,25 @@
 #include <iostream>
+#include <climits>
+
+extern int x;
 
 using namespace std;
 
 int main(){
+    //g++ -Wall -std=c++14 basic.cpp -o basic.exe;
+    //compiler warnings for uninitialized variables;
+    int miles_driven;
+    cout<<"Hello World!"<<miles_driven<<endl;
+    //compilet warnings for unused variabes;
+    int age;
+    // cout<<x; (This is will give linker error)
+    cerr<<"This is an exception"<<endl;
+    char ch;
+    char16_t ch2;
+    char32_t ch3;
+    wchar_t ch4;
 
-
+    int x;
+    cout<<sizeof x;
     return 0;
 }
